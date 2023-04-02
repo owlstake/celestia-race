ac7c8f56156a94edfb6a89c6574caf2053e600a8
owlstake.com <107361397+owlstake@users.noreply.github.com>
2023-04-02 15:11:29 +0700

    Create node_exporter.sh

---
+++ BNT-node-monitoring/node_exporter.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+echo -e "\e[1m\e[32m2. Installing node-exporter... \e[0m" && sleep 1
+# install node-exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+tar xvfz node_exporter-*.*-amd64.tar.gz
+sudo mv node_exporter-*.*-amd64/node_exporter /usr/local/bin/
+rm node_exporter-* -rf
+
+sudo useradd -rs /bin/false node_exporter
+
+sudo tee <<EOF >/dev/null /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
+
+echo -e "\e[1m\e[32mInstallation finished... \e[0m" && sleep 1
+echo -e "\e[1m\e[32mPlease make sure ports 9100 and 9300 are open \e[0m" && sleep 1
