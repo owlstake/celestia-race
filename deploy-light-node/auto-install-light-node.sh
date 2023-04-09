43281c909d72f5085987105547b9720fef9b7cc7
owlstake.com <107361397+owlstake@users.noreply.github.com>
2023-04-09 10:14:33 +0700

    Update auto-install-light-node.sh

--- deploy-light-node/auto-install-light-node.sh
+++ deploy-light-node/auto-install-light-node.sh
@@ -14,9 +14,9 @@ wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go 
 sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" 
 rm "go$ver.linux-amd64.tar.gz"
-go version
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
 source $HOME/.bash_profile
+go version
 echo "Install golang finished"
 
 # Install Celestia-node
@@ -24,7 +24,7 @@ cd $HOME
 rm -rf celestia-node 
 git clone https://github.com/celestiaorg/celestia-node.git 
 cd celestia-node/ 
-git checkout tags/v0.8.1 
+git checkout tags/v0.8.2
 make build 
 make install 
 make cel-key
