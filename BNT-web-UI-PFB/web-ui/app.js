3ea1188a7b2ea4fc390657d21939192ee3f8cea6
owlstake.com <betterman311@gmail.com>
2023-04-24 22:48:17 +0700

    update

--- BNT-web-UI-PFB/web-ui/app.js
+++ BNT-web-UI-PFB/web-ui/app.js
@@ -4,13 +4,31 @@ const path = require('path');
 const flash = require('connect-flash');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const crypto = require('crypto');
+const seedrandom = require('seedrandom');
 
 require('dotenv').config()
 
 const app = express();
 
+const generateRd = () => {
+    const seed = crypto.randomInt(1, 1000);
+    const random = crypto.createHash('sha256').update(seed.toString()).digest('hex');
+    const rand = parseInt(random.slice(0,16), 16);
+    const rng = seedrandom(rand);
+
+    // generate a random namespace ID
+    const nID = crypto.randomBytes(8).toString('hex');
+
+    // generate a random hex-encoded message
+    const lenMsg = Math.floor(rng() * 100);
+    const msg = crypto.randomBytes(lenMsg).toString('hex');
+
+    return { namespace_id: nID, hex: msg }
+}
+
 const app_domain = process.env.APP_DOMAIN;
-const random_api = process.env.RANDOM_API;
+// const random_api = process.env.RANDOM_API;
 const api_url_default = process.env.API_URL_DEFAULT
 
 const publicDir = path.join(__dirname, './public')
@@ -35,11 +53,8 @@ app.post('/submit_pfb', (req, res) => {
 
 app.get('/generate', async (req, res) => {
     try {       
-        const response = await axios.get(
-            random_api,            
-            { timeout: 30000 }
-        )
-        return res.status(200).json(response.data)
+        const rd = generateRd()
+        return res.status(200).json(rd)
     } catch (err) {
         return res.status(500).json({ message: 'Error' })
     }
@@ -56,14 +71,47 @@ app.get('/', (req, res) => {
         height: data.height, 
         txhash: data.txhash,
         show_response: Boolean(data.height || data.txhash),
+        show_response_retrieve: Boolean(data.r_res_height && data.r_res_shares),
+        r_namespace_id: data.namespace_id || data.r_namespace_id,
+        r_height: data.r_height || data.height,
         transaction_url: data.transaction_url,
         data_hash: data.data_hash, 
         namespace_id: data.namespace_id,
         gas_limit: data.gas_limit,
         fee: data.fee,
         api_url: data.api_url,
-        error_message: req.flash('error_message')
+        error_message: req.flash('error_message'),
+        r_res_height: data.r_res_height,
+        r_res_shares: data.r_res_shares,
     })
+})
+
+app.post('/get-namespaced-shares', async function(req, res) {
+    const { r_namespace_id, r_height } = req.body;
+
+    if (!r_height || !r_namespace_id) {
+        req.flash('error_message', 'height and namespace_id is required')
+        return res.redirect('/')
+    }
+
+    try {        
+        const response = await axios.get(`${api_url_default}/namespaced_shares/${r_namespace_id}/height/${r_height}`)
+
+        req.flash('data', JSON.stringify({
+            r_res_shares: response.data.shares, 
+            r_res_height: response.data.height,
+            r_namespace_id,
+            r_height
+        }))
+        return res.redirect('/')
+    } catch (err) {
+        req.flash('error_message', 'Exception error')
+        req.flash('data', JSON.stringify({
+            r_namespace_id,
+            r_height           
+        }))      
+        return res.redirect('/')
+    }
 })
 
 app.post('/', async (req, res) => {   
